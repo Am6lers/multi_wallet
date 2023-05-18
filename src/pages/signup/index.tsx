350a555f3f3e412082a5697a196a3b4c4c84ce2b
YimKeul <leesjun29@gmail.com>
2023-05-18 19:53:19 +0900

    chore

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const SignUp = () => {
-  return <View></View>;
+  return (
+    <SafeAreaView>
+      <Text>Signup</Text>
+    </SafeAreaView>
+  );
 };
 
 export default SignUp;
