4f584124ad2694f436671b4e21d47e02906ebb8c
YimKeul <leesjun29@gmail.com>
2023-05-20 14:48:37 +0900

    feat : edit reat-native-vector-icons

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,10 +2,15 @@ import { ParamListBase, useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Main = () => {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Icon name="delete" size={32} color="red" />
+    </View>
+  );
 };
 
 export default Main;
