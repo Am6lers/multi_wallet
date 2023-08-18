4ab73f9c66bdbc6d493e115308caefa6817d44c8
jwoo9928 <astro9928@o.cnu.ac.kr>
2023-08-19 00:02:19 +0900

    feat: translate

--- src/components/screens/auth/index.tsx
+++ src/components/screens/auth/index.tsx
@@ -4,15 +4,19 @@ import { CIPHER_WITH_TITLE } from '@assets/images/roots';
 import Colors from '@constants/colors';
 import { StyleSheet } from 'react-native';
 import CommonButton from '@components/atoms/CommonButton';
+import TL from '@translate/index';
 
 const Auth = () => {
   return (
     <View useSafeArea={true} style={styles.container}>
       <Image source={CIPHER_WITH_TITLE} style={styles.img} />
       <View style={styles.buttons}>
-        <CommonButton lable={'Press'} size={Button.sizes.large} />
         <CommonButton
-          lable={'Press'}
+          lable={TL.t('initial.auth.create')}
+          size={Button.sizes.large}
+        />
+        <CommonButton
+          lable={TL.t('initial.auth.import')}
           size={Button.sizes.large}
           reverse={true}
           style={styles.button}
---
+++ src/translate/resources/index.ts
@@ -0,0 +1,9 @@
+import en from './en';
+import ko from './ko';
+
+const resources = {
+  en: en,
+  ko: ko,
+};
+
+export default resources;
