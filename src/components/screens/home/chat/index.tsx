d1a6a17306faaa9a7fa497bae7796796401964ba
jwoo9928 <astro9928@o.cnu.ac.kr>
2023-09-22 20:29:37 +0900

    feat: prices & transaction controlelr

--- src/components/screens/home/chat/index.tsx
+++ src/components/screens/home/chat/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import Colors from '@constants/colors';
 import { View } from 'react-native-ui-lib';
 import Constants from '@constants/app';
+import engine from '@core/engine';
 
 const Chat = () => {
+  const { NetworkController } = engine.context;
+  useEffect(() => {}, []);
+
   return <View style={styles.outline} useSafeArea></View>;
 };
 
