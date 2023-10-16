2e724179e293e83789343cdd57a64789ff1ec96f
MoonGoon <moongoon.cnu@gmail.com>
2023-10-16 11:28:41 +0900

    update backup

--- src/components/screens/home/backup/index.tsx
+++ src/components/screens/home/backup/index.tsx
@@ -1,11 +1,20 @@
 import { Text, View } from 'react-native-ui-lib';
 import { StyleSheet } from 'react-native';
 import Colors from '@constants/colors';
+import Header from '@components/atoms/Header';
 
 const Backup = () => {
   return (
-    <View useSafeArea={true} style={styles.container}>
-      <Text>Backup</Text>
+    <View style={styles.outline} useSafeArea>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={{ zIndex: 2 }}>
+            <Header title="지갑 백업하기" />
+          </View>
+          <View></View>
+        </View>
+        <Text>Backup</Text>
+      </View>
     </View>
   );
 };
@@ -16,5 +25,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.White,
+  },
+  outline: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    zIndex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
